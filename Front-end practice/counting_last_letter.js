9e402465aaf0b12aeb88c4612dde4447a21ef5fa
InnocentBoy <141760554+InnocentBoy-007@users.noreply.github.com>
2024-05-19 22:56:51 +0530

    Add files via upload

---
+++ Front-end practice/counting_last_letter.js
@@ -0,0 +1,20 @@
+const word = document.getElementsByClassName("letter");
+
+const counting_letters_last_word = function (word) {
+    const splitted_word = word.split(" ");
+    console.log(splitted_word);
+    const last_word = splitted_word[splitted_word.length - 1];
+    return {
+        length: last_word.length,
+        word: last_word,
+    };
+}
+
+const control_function = function () {
+    for (let i = 0; i < word.length; i++) {
+        const words = word[i];
+        console.log(counting_letters_last_word(words.textContent));
+    }
+}
+
+control_function();
